66b0efaf85472782aa01997c721db49c2682bc8b
Parvaneh Pouladzadeh <parvaneh@Parvanehs-MacBook-Pro.local>
2020-09-01 19:35:33 -0400

    broken homepage to smaller components, and finished styling menu-item component

---
+++ src/pages/homepage/homepage.componet.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Directory from "../../components/directory/directory.component";
+
+import "./homepage.styles.scss";
+// functional component because we don't need to use a ny state here
+// we have one container for menu and one actual menu-item
+// and then a contents to hold the texts
+const HomePage = () => (
+  <div className="homepage">
+    <Directory />
+  </div>
+);
+
+export default HomePage;
